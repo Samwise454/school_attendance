a74de6146d461ac4375be1854eddee2dff99284f
Sam C <chuquest.work@gmail.com>
2026-01-31 22:14:54 +0100

    Minor updates

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -22,7 +22,7 @@ const Nav = () => {
         </div>
         
         <div className="flex-none ml-4">
-            <button onClick={logout} className="btn bg-red-600 px-2 mr-3 text-white">
+            <button onClick={logout} className="btn bg-white border-0 px-2 mr-3 text-white">
                 {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg> */}
                 Logout
             </button>
